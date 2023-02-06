52a270b08db5972cf423e6eac517c42b4cb48c4e
matteggers <94013259+matteggers@users.noreply.github.com>
2023-02-05 21:38:08 -0500

    first commit, broken code.

---
+++ test.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+// basic volume calculator
+
+const float = 3.149567
+char userChoice = '';
+
+
+int main() {
+    cout << "What is the shape of the object you want to find volume for? P: Pyramid, R: Rectangular Prism, S: Sphere.";
+
+
+}
+
+
+switch (expression)
+{
+case /* constant-expression */:
+    /* code */
+    break;
+
+default:
+    break;
+}
